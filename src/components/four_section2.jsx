0066a7a93e26da1dc541c2cc451ea681d7ff7709
kidus Tesfaye <kidus.z3rd@gmail.com>
2024-10-02 08:49:54 +0300

    update: four section images updated

--- src/components/four_section2.jsx
+++ src/components/four_section2.jsx
@@ -7,8 +7,8 @@ import Image03 from "../assets/images/new/four/img03.png";
 import Image05 from "../assets/images/new/four/img04.png";
 import Image04 from "../assets/images/four/img04.jpg";
 import Image06 from "../assets/images/new/four/img05.png";
-import Image07 from "../assets/images/new/four/img06.png";
-import Image08 from "../assets/images/new/four/img07.png";
+import Image07 from "../assets/images/new/four/img09.jpg";
+import Image08 from "../assets/images/new/four/img10.jpg";
 
 //import Image02 from "../assets/images/four/img02.jpg";
 //import Image03 from "../assets/images/four/img03.jpg";
